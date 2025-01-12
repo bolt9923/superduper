261ead43f54d3a83ec46976730615cacc0a718f2
UTTAM RATHORE <uk977061@gmail.com>
2025-01-12 17:55:10 +0530

    Update start.py

--- BABYMUSIC/plugins/bot/start.py
+++ BABYMUSIC/plugins/bot/start.py
@@ -18,6 +18,9 @@ from BABYMUSIC.utils.database import (
     get_lang,
     is_banned_user,
     is_on_off,
+    save_user,
+    get_user_data,
+    update_referrer,
 )
 from BABYMUSIC.utils.decorators.language import LanguageStart
 from BABYMUSIC.utils.formatters import get_readable_time
@@ -39,28 +42,38 @@ YUMI_PICS = [
 @LanguageStart
 async def start_pm(client, message: Message, _):
     await add_served_user(message.from_user.id)
+    user_id = message.from_user.id
+    mention = message.from_user.mention
+
+    # Handle if the message contains extra text
     if len(message.text.split()) > 1:
-        name = message.text.split(None, 1)[1]
-        if name[0:4] == "help":
+        name = message.text.split(None, 1)[1].lower()
+
+        # Handle the 'help' command
+        if name.startswith("help"):
             keyboard = help_pannel(_)
-            return await message.reply_photo(
-                random.choice(YUMI_PICS),
-                caption=_["help_1"].format(config.SUPPORT_CHAT),
+            return await message.send_message(
+                text=_["help_1"].format(config.SUPPORT_CHAT),
                 reply_markup=keyboard,
             )
-        if name[0:3] == "sud":
+
+        # Handle the 'sud' command
+        elif name.startswith("sud"):
             await sudoers_list(client=client, message=message, _=_)
             if await is_on_off(2):
                 return await app.send_message(
                     chat_id=config.LOGGER_ID,
-                    text=f"✦ {message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <b>sᴜᴅᴏʟɪsᴛ</b>.\n\n<b>✦ ᴜsᴇʀ ɪᴅ ➠</b> <code>{message.from_user.id}</code>\n<b>✦ ᴜsᴇʀɴᴀᴍᴇ ➠</b> @{message.from_user.username}",
+                    text=f"✦ {mention} just started the bot to check <b>sudolist</b>.\n\n<b>✦ User ID ➠</b> <code>{user_id}</code>\n<b>✦ Username ➠</b> @{message.from_user.username}",
                 )
             return
-        if name[0:3] == "inf":
+
+        # Handle the 'inf' command
+        elif name.startswith("inf"):
             m = await message.reply_text("🔎")
-            query = (str(name)).replace("info_", "", 1)
+            query = name.replace("info_", "", 1)
             query = f"https://www.youtube.com/watch?v={query}"
             results = VideosSearch(query, limit=1)
+
             for result in (await results.next())["result"]:
                 title = result["title"]
                 duration = result["duration"]
@@ -70,6 +83,7 @@ async def start_pm(client, message: Message, _):
                 channel = result["channel"]["name"]
                 link = result["link"]
                 published = result["publishedTime"]
+
             searched_text = _["start_6"].format(
                 title, duration, views, published, channellink, channel, app.mention
             )
@@ -88,24 +102,61 @@ async def start_pm(client, message: Message, _):
                 caption=searched_text,
                 reply_markup=key,
             )
+
             if await is_on_off(2):
                 return await app.send_message(
                     chat_id=config.LOGGER_ID,
-                    text=f"✦ {message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <b>ᴛʀᴀᴄᴋ ɪɴғᴏʀᴍᴀᴛɪᴏɴ</b>.\n\n✦ <b>ᴜsᴇʀ ɪᴅ ➠</b> <code>{message.from_user.id}</code>\n✦ <b>ᴜsᴇʀɴᴀᴍᴇ ➠</b> @{message.from_user.username}",
+                    text=f"✦ {mention} just started the bot to check <b>track information</b>.\n\n✦ <b>User ID ➠</b> <code>{user_id}</code>\n✦ <b>Username ➠</b> @{message.from_user.username}",
                 )
-    else:
-        out = private_panel(_)
-        await message.reply_photo(
-            random.choice(YUMI_PICS),
-            caption=_["start_2"].format(message.from_user.mention, app.mention),
-            # caption=_["start_2"].format(message.from_user.mention, app.mention, UP, DISK, CPU, RAM,served_users,served_chats),
-            reply_markup=InlineKeyboardMarkup(out),
+
+        # Handle referral logic
+        elif name.isdigit():
+            referrer_id = int(name)
+            referrer = await get_user_data(referrer_id)
+            if referrer and referrer_id != user_id:
+                await update_referrer(referrer_id)
+                # Send message to the referrer
+                await app.send_message(
+                    referrer_id,
+                    text=f"🎉 New user {mention} joined using your referral link!",
+                )
+
+    # Check if the user already exists in the database
+    user_data = await get_user_data(user_id)
+    if not user_data:
+        await save_user(user_id)
+        user_data = await get_user_data(user_id)
+
+    # Fetch user data
+    points = user_data.get("points", 0)
+    referrals = user_data.get("referrals", 0)
+
+    # Generate referral link
+    referral_link = f"https://t.me/{client.me.username}?start={user_id}"
+
+    # Send user dashboard
+    response = f"""**Hey {mention} 👋**
+**This is 𝐘ᴛ-𝐌ᴜsɪᴄ**
+**The best music|video streaming on VC**
+
+**Your points:** {points}
+**Referrals:** {referrals}
+**User:** [regular]
+
+**Your Referral Link:** [Click Here]({referral_link})"""
+
+    await message.reply_text(
+        text=response,
+        reply_markup=ReplyKeyboardMarkup(CHOICE, resize_keyboard=True),
+        disable_web_page_preview=True,
+    )
+
+    # Log the start event if enabled
+    if await is_on_off(2):
+        await app.send_message(
+            chat_id=config.LOGGER_ID,
+            text=f"✦ {mention} just started the bot.\n\n✦ <b>User ID ➠</b> <code>{user_id}</code>\n✦ <b>Username ➠</b> @{message.from_user.username}",
         )
-        if await is_on_off(2):
-            return await app.send_message(
-                chat_id=config.LOGGER_ID,
-                text=f"✦ {message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ.\n\n✦ <b>ᴜsᴇʀ ɪᴅ ➠</b> <code>{message.from_user.id}</code>\n✦ <b>ᴜsᴇʀɴᴀᴍᴇ ➠</b> @{message.from_user.username}",
-            )
 
 
 @app.on_message(filters.command(["start"]) & filters.group & ~BANNED_USERS)
