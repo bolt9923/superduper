c3843a43831694c1e95450128783aae66f11e668
✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>
2025-01-14 02:03:44 +0530

    Update block.py

--- BABYMUSIC/plugins/sudo/block.py
+++ BABYMUSIC/plugins/sudo/block.py
@@ -1,11 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from SONALI import app
-from SONALI.misc import SUDOERS
-from SONALI.utils.database import add_gban_user, remove_gban_user
-from SONALI.utils.decorators.language import language
-from SONALI.utils.extraction import extract_user
+from BABYMUSIC import app
+from BABYMUSIC.misc import SUDOERS
+from BABYMUSIC.utils.database import add_gban_user, remove_gban_user
+from BABYMUSIC.utils.decorators.language import language
+from BABYMUSIC.utils.extraction import extract_user
 from config import BANNED_USERS
 
 
