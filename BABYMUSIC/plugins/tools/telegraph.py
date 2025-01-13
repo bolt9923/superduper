f951cbb215e6db041700c7c0a52a06fb145698ac
✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>
2025-01-13 20:11:38 +0530

    Create telegraph.py

---
+++ BABYMUSIC/plugins/tools/telegraph.py
@@ -0,0 +1,86 @@
+import os
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from BABYMUSIC import app
+import requests
+
+
+def upload_file(file_path):
+    url = "https://catbox.moe/user/api.php"
+    data = {"reqtype": "fileupload", "json": "true"}
+    files = {"fileToUpload": open(file_path, "rb")}
+    response = requests.post(url, data=data, files=files)
+
+    if response.status_code == 200:
+        return True, response.text.strip()
+    else:
+        return False, f"ᴇʀʀᴏʀ: {response.status_code} - {response.text}"
+
+
+@app.on_message(filters.command(["tgm", "tgt", "telegraph", "tl"]))
+async def get_link_group(client, message):
+    if not message.reply_to_message:
+        return await message.reply_text(
+            "Reply to a media to upload on Telegraph"
+        )
+
+    media = message.reply_to_message
+    file_size = 0
+    if media.photo:
+        file_size = media.photo.file_size
+    elif media.video:
+        file_size = media.video.file_size
+    elif media.document:
+        file_size = media.document.file_size
+
+    if file_size > 200 * 1024 * 1024:
+        return await message.reply_text("Please provide a media file under 200MB.")
+
+    try:
+        text = await message.reply("Processing...")
+
+        async def progress(current, total):
+            try:
+                await text.edit_text(f"Downloading... {current * 100 / total:.1f}%")
+            except Exception:
+                pass
+
+        try:
+            local_path = await media.download(progress=progress)
+            await text.edit_text("Uploading to Telegraph...")
+
+            success, upload_path = upload_file(local_path)
+
+            if success:
+                await text.edit_text(
+                    f"🔗 |Success Upload [Link]({upload_path})",
+                    reply_markup=InlineKeyboardMarkup(
+                        [
+                            [
+                                InlineKeyboardButton(
+                                    "Copy link 🔗",
+                                    url=upload_path,
+                                )
+                            ]
+                        ]
+                    ),
+                )
+            else:
+                await text.edit_text(
+                    f"An error occurred while uploading your file\n{upload_path}"
+                )
+
+            try:
+                os.remove(local_path)
+            except Exception:
+                pass
+
+        except Exception as e:
+            await text.edit_text(f"File upload failed\n\n<i>reasion: {e}</i>")
+            try:
+                os.remove(local_path)
+            except Exception:
+                pass
+            return
+    except Exception:
+        pass
