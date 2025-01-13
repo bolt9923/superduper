aac2ad93d2fe8ba194c7c109484b25ad5706e524
✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>
2025-01-14 00:27:09 +0530

    Update vctools.py

--- BABYMUSIC/plugins/tools/vctools.py
+++ BABYMUSIC/plugins/tools/vctools.py
@@ -1,13 +1,13 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from SONALI import app
+from BABYMUSIC import app
 from pyrogram import *
 from pyrogram.types import *
 from config import OWNER_ID
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram.raw.functions.phone import CreateGroupCall, DiscardGroupCall
 from pyrogram.raw.types import InputGroupCall
-from SONALI.utils.database import get_assistant
+from BABYMUSIC.utils.database import get_assistant
 from telethon.tl.functions.phone import (
     CreateGroupCallRequest,
     DiscardGroupCallRequest,
