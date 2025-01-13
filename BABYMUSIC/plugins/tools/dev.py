23822975f94b444ffae1dcebc3beef06b9b9c38f
✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>
2025-01-14 00:11:54 +0530

    Update dev.py

--- BABYMUSIC/plugins/tools/dev.py
+++ BABYMUSIC/plugins/tools/dev.py
@@ -11,7 +11,7 @@ from time import time
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
-from SONALI import app
+from BABYMUSIC import app
 from config import OWNER_ID
 ###########
 
