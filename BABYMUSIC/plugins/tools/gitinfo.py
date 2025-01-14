fae1011cb2044e109f6c85c895a876f16a019b83
✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>
2025-01-14 20:21:57 +0530

    Update gitinfo.py

--- BABYMUSIC/plugins/tools/gitinfo.py
+++ BABYMUSIC/plugins/tools/gitinfo.py
@@ -7,7 +7,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 ####
 
-@app.on_message(filters.command(["github", "git"]))
+@app.on_message(filters.command(["github", "git", "gitinfo"]))
 async def github(_, message):
     if len(message.command) != 2:
         await message.reply_text("/git BABYMUSIC")
