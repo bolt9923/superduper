21924b7c55858f23fd3d58005a57d0f48d8a6c95
✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>
2025-01-15 03:00:43 +0530

    Update mention.py

--- BABYMUSIC/plugins/admins/mention.py
+++ BABYMUSIC/plugins/admins/mention.py
@@ -1,8 +1,8 @@
 import asyncio
 from pyrogram.enums import ChatType, ChatMemberStatus
-from SONALI import app
+from BABYMUSIC import app
 from pyrogram import filters
-from SONALI.utils.RAUSHAN_ban import admin_filter
+from BABYMUSIC.utils.UTTAM_ban import admin_filter
 
 
 
