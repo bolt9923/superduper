0e149c5e257b90baa423c500ff4505b1ef6c8aed
✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>
2025-01-14 23:45:05 +0530

    Update bots.py

--- BABYMUSIC/plugins/tools/bots.py
+++ BABYMUSIC/plugins/tools/bots.py
@@ -1,29 +1,76 @@
+import os
 import asyncio
-
-from pyrogram import enums, filters
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from pyrogram import enums
+from pyrogram.enums import ChatMemberStatus
 from pyrogram.errors import FloodWait
-
 from BABYMUSIC import app
 
 
-@app.on_message(filters.command("bots") & filters.group)
-async def bots(client, message):
-
+@app.on_message(filters.command(["admins","staff"]))
+async def admins(client, message):
+  try: 
+    adminList = []
+    ownerList = []
+    async for admin in app.get_chat_members(message.chat.id, filter=enums.ChatMembersFilter.ADMINISTRATORS):
+      if admin.privileges.is_anonymous == False:
+        if admin.user.is_bot == True:
+          pass
+        elif admin.status == ChatMemberStatus.OWNER:
+          ownerList.append(admin.user)
+        else:  
+          adminList.append(admin.user)
+      else:
+        pass   
+    lenAdminList= len(ownerList) + len(adminList)  
+    text2 = f"**Group staff - {message.chat.title}**\n\n"
     try:
-        botList = []
-        async for bot in app.get_chat_members(
-            message.chat.id, filter=enums.ChatMembersFilter.BOTS
-        ):
-            botList.append(bot.user)
-        lenBotList = len(botList)
-        text3 = f"**Bots list - {message.chat.title}**\n\nBots\n"
-        while len(botList) > 1:
-            bot = botList.pop(0)
-            text3 += f"├ @{bot.username}\n"
+      owner = ownerList[0]
+      if owner.username == None:
+        text2 += f"👑 Owner\n└ {owner.mention}\n\n👮🏻 Admin\n"
+      else:
+        text2 += f"👑 Owner\n└ @{owner.username}\n\n👮🏻 Admin\n"
+    except:
+      text2 += f"👑 Owner\n└ <i>Hidden</i>\n\n👮🏻 Admin\n"
+    if len(adminList) == 0:
+      text2 += "└ <i>Admins are hidden</i>"  
+      await app.send_message(message.chat.id, text2)   
+    else:  
+      while len(adminList) > 1:
+        admin = adminList.pop(0)
+        if admin.username == None:
+          text2 += f"├ {admin.mention}\n"
         else:
-            bot = botList.pop(0)
-            text3 += f"└ @{bot.username}\n\n"
-            text3 += f"**Total number of bots**: {lenBotList}**"
-            await app.send_message(message.chat.id, text3)
-    except FloodWait as e:
-        await asyncio.sleep(e.value)
+          text2 += f"├ @{admin.username}\n"    
+      else:    
+        admin = adminList.pop(0)
+        if admin.username == None:
+          text2 += f"└ {admin.mention}\n\n"
+        else:
+          text2 += f"└ @{admin.username}\n\n"
+      text2 += f"✅ | **Total number of admins**: {lenAdminList}"  
+      await app.send_message(message.chat.id, text2)           
+  except FloodWait as e:
+    await asyncio.sleep(e.value)       
+
+# ------------------------------------------------------------------------------- #
+
+@app.on_message(filters.command("bots"))
+async def bots(client, message):  
+  try:    
+    botList = []
+    async for bot in app.get_chat_members(message.chat.id, filter=enums.ChatMembersFilter.BOTS):
+      botList.append(bot.user)
+    lenBotList = len(botList) 
+    text3  = f"**Bot list - {message.chat.title}**\n\n🤖 Bots\n"
+    while len(botList) > 1:
+      bot = botList.pop(0)
+      text3 += f"├ @{bot.username}\n"    
+    else:    
+      bot = botList.pop(0)
+      text3 += f"└ @{bot.username}\n\n"
+      text3 += f"✅ | *Total number of bots**: {lenBotList}"  
+      await app.send_message(message.chat.id, text3)
+  except FloodWait as e:
+    await asyncio.sleep(e.value)
