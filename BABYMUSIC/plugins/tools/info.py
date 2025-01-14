54b06ec6fc0598f5183380cff110c46b74c36d15
✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>
2025-01-14 20:49:39 +0530

    Update info.py

--- BABYMUSIC/plugins/tools/info.py
+++ BABYMUSIC/plugins/tools/info.py
@@ -9,6 +9,10 @@ from BABYMUSIC.utils.database import clonebotdb # Replace with the actual module
 DEFAULT_IMG = 'https://files.catbox.moe/01i7ch.jpg'  # Replace with the actual URL or file ID
 
 
+
+def escape_markdown(text: str) -> str:
+    return re.sub(r'([_\*`\[\]()~>#+-=|{}.!])', r'\\\1', text)
+
 @app.on_message(filters.command('info'))
 async def user_info(client: Client, message: Message):
     if len(message.command) < 2:
@@ -24,7 +28,7 @@ async def user_info(client: Client, message: Message):
         user_id = user.id
         first_name = user.first_name or "N/A"
         username = f"@{user.username}" if user.username else "N/A"
-        user_link = f"[Click](tg://user?id={user_id})"
+        user_link = f"[Link to Profile](tg://user?id={user_id})"
 
         # Check user status based on the database
         cloned_bots = clonebotdb.find({"user_id": user_id})  # Query the collection
@@ -34,6 +38,12 @@ async def user_info(client: Client, message: Message):
             user_status = "[VIP 💳]"
         else:
             user_status = "[Regular 👥]"
+
+        # Escape special characters for Markdown compatibility
+        user_status = escape_markdown(user_status)
+        first_name = escape_markdown(first_name)
+        username = escape_markdown(username)
+        user_link = escape_markdown(user_link)
 
         # User photo (using `client.get_profile_photos` to fetch the user's profile photo)
         profile_photos = await client.get_profile_photos(user.id)
