52e04da3fdf2bbcff3d743f9fd99161eac449ded
✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>
2025-01-14 21:06:15 +0530

    Update info.py

--- BABYMUSIC/plugins/tools/info.py
+++ BABYMUSIC/plugins/tools/info.py
@@ -25,8 +25,8 @@ async def user_info(client: Client, message: Message):
         
         # User details
         user_id = user.id
-        first_name = user.first_name or "N/A"
-        username = f"@{user.username}" if user.username else "N/A"
+        first_name = escape_markdown(user.first_name or "N/A")
+        username = escape_markdown(f"@{user.username}" if user.username else "N/A")
         user_link = f"[Link to Profile](tg://user?id={user_id})"
 
         # Check user status based on the database
@@ -38,11 +38,7 @@ async def user_info(client: Client, message: Message):
         else:
             user_status = "[Regular 👥]"
 
-        # Escape special characters for Markdown compatibility
         user_status = escape_markdown(user_status)
-        first_name = escape_markdown(first_name)
-        username = escape_markdown(username)
-        user_link = escape_markdown(user_link)
 
         # User photo (using `client.get_profile_photos` to fetch the user's profile photo)
         profile_photos = await app.get_profile_photos(user.id)
