a0e4dde855abfe8eda446b715359c90b99750a20
✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>
2025-01-15 21:12:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ load_dotenv()
 API_ID = int(getenv("API_ID", "16457832"))
 API_HASH = getenv("API_HASH", "3030874d0befdb5d05597deacc3e83ab")
 # ------------------------------------------------------
-BOT_TOKEN = getenv("BOT_TOKEN", "7638229482:AAHYmaiKh5Xla91GPdZiLMEsFCN_N-iJfv8")
+BOT_TOKEN = getenv("BOT_TOKEN", "7638229482:AAGhnv5mzO_7nmeccUjgwNURnLkc_N5ETbs")
 # -------------------------------------------------------
 OWNER_USERNAME = getenv("OWNER_USERNAME","untold_coder")
 # --------------------------------------------------------
