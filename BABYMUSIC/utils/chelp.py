7cf67582b78b484901217ec243858c450b689ec8
✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>
2025-01-15 20:04:11 +0530

    Update chelp.py

--- BABYMUSIC/utils/chelp.py
+++ BABYMUSIC/utils/chelp.py
@@ -9,23 +9,27 @@ def first_page(_):
         [
             [
                 InlineKeyboardButton(text=_["H_B_1"], callback_data="help_callback hb1"),
-                InlineKeyboardButton(text=_["H_B_10"], callback_data="help_callback hb2"),
+                InlineKeyboardButton(text=_["H_B_10"], callback_data="help_callback hb10"),
             ],
             [
-                InlineKeyboardButton(text=_["H_B_11"], callback_data="help_callback hb3"),
-                InlineKeyboardButton(text=_["H_B_14"], callback_data="help_callback hb4"),
+                InlineKeyboardButton(text=_["H_B_11"], callback_data="help_callback hb11"),
+                InlineKeyboardButton(text=_["H_B_14"], callback_data="help_callback hb14"),
             ],
             [
-                InlineKeyboardButton(text=_["H_B_16"], callback_data="help_callback hb5"),
-                InlineKeyboardButton(text=_["H_B_17"], callback_data="help_callback hb6"),
+                InlineKeyboardButton(text=_["H_B_16"], callback_data="help_callback hb16"),
+                InlineKeyboardButton(text=_["H_B_17"], callback_data="help_callback hb17"),
             ],
             [
                 InlineKeyboardButton(
-                    text=_["H_B_22"], callback_data="help_callback hb7"
+                    text=_["H_B_22"], callback_data="help_callback hb22"
                 ),
                 InlineKeyboardButton(
                     text="More futures", url="http://t.me/YOUTUBE_RROBOT?start=help"
                 ),
+                [
+                InlineKeyboardButton(
+                    text="Close", callback_data="close"
+                )
             ],
             #[
                # InlineKeyboardButton(
