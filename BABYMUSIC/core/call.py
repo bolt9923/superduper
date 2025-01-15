9976af07c906194c581d5da3cc2e246516bb09ac
✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>
2025-01-16 00:32:17 +0530

    Update call.py

--- BABYMUSIC/core/call.py
+++ BABYMUSIC/core/call.py
@@ -17,9 +17,9 @@ from pytgcalls.types.input_stream.quality import HighQualityAudio, MediumQuality
 from pytgcalls.types.stream import StreamAudioEnded
 
 import config
-from BABYMUSI import LOGGER, YouTube, app
-from BABYMUSI.misc import db
-from BABYMUSI.utils.database import (
+from BABYMUSIC import LOGGER, YouTube, app
+from BABYMUSIC.misc import db
+from BABYMUSIC.utils.database import (
     add_active_chat,
     add_active_video_chat,
     get_lang,
@@ -31,11 +31,11 @@ from BABYMUSI.utils.database import (
     remove_active_video_chat,
     set_loop,
 )
-from BABYMUSI.utils.exceptions import AssistantErr
-from BABYMUSI.utils.formatters import check_duration, seconds_to_min, speed_converter
-from BABYMUSI.utils.inline.play import stream_markup, telegram_markup
-from BABYMUSI.utils.stream.autoclear import auto_clean
-from BABYMUSI.utils.thumbnails import get_thumb
+from BABYMUSIC.utils.exceptions import AssistantErr
+from BABYMUSIC.utils.formatters import check_duration, seconds_to_min, speed_converter
+from BABYMUSIC.utils.inline.play import stream_markup, telegram_markup
+from BABYMUSIC.utils.stream.autoclear import auto_clean
+from BABYMUSIC.utils.thumbnails import get_thumb
 from strings import get_string
 
 autoend = {}
