d07f0ee408b6972708d222301fd5860bd161d59b
✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>
2025-01-28 15:17:58 +0530

    Update __init__.py

--- BABYMUSIC/__init__.py
+++ BABYMUSIC/__init__.py
@@ -2,7 +2,7 @@ from BABYMUSIC.core.bot import BABY
 from BABYMUSIC.core.dir import dirr
 from BABYMUSIC.core.git import git
 from BABYMUSIC.core.userbot import Userbot
-from BABYMUSIC.ccore import Cuserbot
+from BABYMUSIC.ccore.Cuserbot import Userbot
 from BABYMUSIC.misc import dbb, heroku
 from pyrogram import Client
 from SafoneAPI import SafoneAPI
@@ -17,6 +17,7 @@ app = BABY()
 api = SafoneAPI()
 userbot = Userbot()
 Cuserbot = start_all_assistants()
+
 from .platforms import *
 
 Apple = AppleAPI()
